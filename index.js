0bb0e22d6664f81737f0a6a81fb6ed20e70442ae
mloginov <mloginov@codio.com>
2023-09-14 15:51:45 +0100

    show only once

--- index.js
+++ index.js
@@ -273,7 +273,7 @@
         // need to add "window.codio = codio;" before "loadJS" in custom scripts
         const BIRTHDAY_KEY = 'codio-guides-linter-birthday'
         localStorage.getItem(BIRTHDAY_KEY)
-        if (window.codio.userId === 'd8e2f318-8d43-4d06-ad2f-28bb7acb2ce1' || localStorage.getItem(BIRTHDAY_KEY) !== '1') {
+        if (window.codio.userId === 'd8e2f318-8d43-4d06-ad2f-28bb7acb2ce1' && localStorage.getItem(BIRTHDAY_KEY) !== '1') {
           localStorage.setItem(BIRTHDAY_KEY, '1')
           addModalContent('<h1 style="padding: 30px 0; text-align: center;">Happy birthday, Chalece!!!</h1>')
        }
