1eb4ace7950ed5dac367f673b422aa89c468dff2
mloginov <mloginov@codio.com>
2024-01-12 15:38:14 +0000

    fixes

--- src/rules/assessment/common/question.js
+++ src/rules/assessment/common/question.js
@@ -1,7 +1,10 @@
-import {RULE_LEVELS} from '../../const'
+import {ASSESSMENT, RULE_LEVELS} from '../../const'
 
 export default {
   action: (assessment) => {
+    if (assessment.type === ASSESSMENT.TYPES.GRADE_BOOK) {
+      return undefined
+    }
     const re = /^(\*\*.+\*\*|### .+)/s
     return !re.test(assessment.source.instructions) ?
       'Question should not be blank. Text should also be bold (**)' : undefined
